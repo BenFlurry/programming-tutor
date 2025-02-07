745bbc972c8e2df419106c1a5bb50b833095b4a8
Ben Alexander <benjaminalexander747@gmail.com>
2025-02-07 10:21:28 +0000

    more trees

--- testing-trees/tests.py
+++ testing-trees/tests.py
@@ -20,17 +20,3 @@ class TestBinaryTree(unittest.TestCase):
             self.tree.insert(key)
         self.assertEqual(self.tree.inorder_traversal(self.tree.root), [3, 5, 7, 10, 12, 15, 18])
 
-    def test_preorder_traversal(self):
-        keys = [10, 5, 15, 3, 7, 12, 18]
-        for key in keys:
-            self.tree.insert(key)
-        self.assertEqual(self.tree.preorder_traversal(self.tree.root), [10, 5, 3, 7, 15, 12, 18])
-
-    def test_postorder_traversal(self):
-        keys = [10, 5, 15, 3, 7, 12, 18]
-        for key in keys:
-            self.tree.insert(key)
-        self.assertEqual(self.tree.postorder_traversal(self.tree.root), [3, 7, 5, 12, 18, 15, 10])
-
-if __name__ == '__main__':
-    unittest.main()
